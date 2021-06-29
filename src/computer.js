fdbfa00daa56da2f13e3c6e7668c73313feeebb4
Michelle <michelle.chiem@gmail.com>
2021-06-29 13:41:03 -0600

    should pass first computer test

--- src/computer.js
+++ src/computer.js
@@ -1,4 +1,11 @@
 class Computer {
+    constructor (name, os, processor, memory, graphics) {
+        this.name = name;
+        this.os = os;
+        this.processor =  processor;
+        this.memory = memory;
+        this.graphics = graphics;
+    }
   
 }
 
